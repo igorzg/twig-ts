179280b2d6170bd1d4440690d2eb967ec3754899
igorzg <igor.zg1987@gmail.com>
2016-04-17 15:57:11 +0200

    Update of lexical analysis and tests.

--- src/tests/lexer-spec.ts
+++ src/tests/lexer-spec.ts
@@ -1,138 +1,146 @@
 import {Lexer} from '../lexer';
 import {LexerOptions} from '../lexeroptions';
-import {Token} from '../token';
 import {Tokens} from '../tokens';
 import {readFileSync} from 'fs';
-import {isEqual} from '../core';
 import {InvalidTokenError} from '../error';
 import {assert} from 'chai';
-import {inspect} from 'util';
 let layout = readFileSync(__dirname + '/../../tmpl/layout.twig', 'utf-8');
+let layoutTokens = JSON.parse(
+	readFileSync(__dirname + '/../../tmpl/layout-tokens.json', 'utf-8')
+);
 
 describe('Lexer', () => {
 	it('Should give correct lexical analysis of layout.twig', (done) => {
-		let lexer = new Lexer(layout, new LexerOptions());
-		lexer.parse().then(
-			(data) => {
-
-				console.log('data.tokens', data.tokens);
-				assert.equal(data.tokens.length, 55);
-				assert.isTrue(isEqual(data.tokens, [
-					new Token(
-						Tokens.VARIABLE_START,
-						'{{', 2, 8
-					),
-					new Token(
-						Tokens.STRING,
-						'liked_product_notification', 2, 58
-					),
-					new Token(
-						Tokens.VARIABLE_END,
-						'}}', 2, 61
-					),
-					new Token(
-						Tokens.BLOCK_START,
-						'{%', 3, 8
-					),
-					new Token(
-						Tokens.STRING,
-						'first-time-seen', 3, 34
-					),
-					new Token(
-						Tokens.BLOCK_END,
-						'%}', 3, 36
-					),
-					new Token(
-						Tokens.BLOCK_START,
-						'{%-', 4, 9
-					),
-					new Token(
-						Tokens.STRING,
-						'abc', 4, 23
-					),
-					new Token(
-						Tokens.BLOCK_END,
-						'-%}', 4, 26
-					),
-					new Token(
-						Tokens.COMMENT_START,
-						'{#', 5, 8
-					),
-					new Token(
-						Tokens.COMMENT_END,
-						'#}', 7, 8
-					)
-				]));
+		let t1 = (new Date()).getTime();
+		let lexer1 = new Lexer(layout, new LexerOptions());
+		Promise.all([
+				lexer1.parse().then(
+					(data) => {
+						assert.deepEqual(data.tokens, layoutTokens);
+						assert.equal(data.tokens.length, 250);
+					}
+				)
+			])
+			.then((data) => {
+				let t2 = (new Date()).getTime();
+				console.log('TIME', +(t2 - t1));
 				done();
-			}
-		).catch(done);
+			})
+			.catch((error) => {
+				let t2 = (new Date()).getTime();
+				console.log('TIME IN ERROR', +(t2 - t1));
+				done(error);
+			});
 	});
 
 
-	it('Should open and close tokens correctly', (done) => {
-		let template = `<div data-cn-body-wrapper class="cn-body-wrapper">
-			    {{ macro.flash_message('liked_product_notification') }}
-			    {% widget 'first-time-seen' %}
-			    {%- widget 'abc' -%}
-			    {# this is an comment {%%} {{   test  }}
-			       <div some bullshit></div>
-			    #}
-		    </div>
-    	`;
+	it('Should open and close block', (done) => {
+		let template = `<div>
+			     {% %}
+		</div>`;
 		let lexer = new Lexer(template, new LexerOptions());
 		lexer.parse().then(
 			(data) => {
-				assert.isTrue(isEqual(data.tokens, [
-					new Token(
-						Tokens.VARIABLE_START,
-						'{{', 2, 8
-					),
-					new Token(
-						Tokens.STRING,
-						'liked_product_notification', 2, 58
-					),
-					new Token(
-						Tokens.VARIABLE_END,
-						'}}', 2, 61
-					),
-					new Token(
-						Tokens.BLOCK_START,
-						'{%', 3, 8
-					),
-					new Token(
-						Tokens.STRING,
-						'first-time-seen', 3, 34
-					),
-					new Token(
-						Tokens.BLOCK_END,
-						'%}', 3, 36
-					),
-					new Token(
-						Tokens.BLOCK_START,
-						'{%-', 4, 9
-					),
-					new Token(
-						Tokens.STRING,
-						'abc', 4, 23
-					),
-					new Token(
-						Tokens.BLOCK_END,
-						'-%}', 4, 26
-					),
-					new Token(
-						Tokens.COMMENT_START,
-						'{#', 5, 8
-					),
-					new Token(
-						Tokens.COMMENT_END,
-						'#}', 7, 8
-					)
-				]));
+				assert.deepEqual(data.tokens, [
+					{
+						columnNumber: 9,
+						lineNumber: 2,
+						tokenName: 'BLOCK_START',
+						type: Tokens.BLOCK_START,
+						value: '{%'
+					},
+					{
+						columnNumber: 10,
+						lineNumber: 2,
+						tokenName: 'WHITESPACE',
+						type: Tokens.WHITESPACE,
+						value: ' '
+					},
+					{
+						columnNumber: 12,
+						lineNumber: 2,
+						tokenName: 'BLOCK_END',
+						type: Tokens.BLOCK_END,
+						value: '%}'
+					}
+				]);
 				done();
 			}
 		).catch(done);
 	});
 
+	it('Should open and close variable', (done) => {
+		let template = `<div>{{ value }}</div>`;
+		let lexer = new Lexer(template, new LexerOptions());
+		lexer.parse().then(
+			(data) => {
+				assert.deepEqual(data.tokens, [
+					{
+						columnNumber: 7,
+						lineNumber: 1,
+						tokenName: 'VARIABLE_START',
+						type: Tokens.VARIABLE_START,
+						value: '{{'
+					},
+					{
+						columnNumber: 8,
+						lineNumber: 1,
+						tokenName: 'WHITESPACE',
+						type: Tokens.WHITESPACE,
+						value: ' '
+					},
+					{
+						columnNumber: 13,
+						lineNumber: 1,
+						tokenName: 'SYMBOL',
+						type: Tokens.SYMBOL,
+						value: 'value'
+					},
+					{
+						columnNumber: 14,
+						lineNumber: 1,
+						tokenName: 'WHITESPACE',
+						type: Tokens.WHITESPACE,
+						value: ' '
+					},
+					{
+						columnNumber: 16,
+						lineNumber: 1,
+						tokenName: 'VARIABLE_END',
+						type: Tokens.VARIABLE_END,
+						value: '}}'
+					}
+				]);
+				done();
+			}
+		).catch(done);
+	});
+	
+	it('Should open and close comment', (done) => {
+		let template = `<div>{# this is some comment {{}} #}</div>`;
+		let lexer = new Lexer(template, new LexerOptions());
+		lexer.parse().then(
+			(data) => {
+				assert.deepEqual(data.tokens, [
+					{
+						columnNumber: 7,
+						lineNumber: 1,
+						tokenName: 'COMMENT_START',
+						type: Tokens.COMMENT_START,
+						value: '{#'
+					},
+					{
+						columnNumber: 36,
+						lineNumber: 1,
+						tokenName: 'COMMENT_END',
+						type: Tokens.COMMENT_END,
+						value: '#}'
+					}
+				]);
+				done();
+			}
+		).catch(done);
+	});
 
 	it('Should throw InvalidOpenTokenError', (done) => {
 		let lexer = new Lexer(`This is an test 
